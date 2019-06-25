9e8711ccceedb2108d711f85a8dd8b0431880417
Kanokporn Namsuwan <52119412+Kanokporn3143@users.noreply.github.com>
2019-06-25 20:35:58 +0700

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name = " Kanokporn Namsuwan ";
+    int scores1 = 100;
+    int scores2 = 79;
+    float GPA = 3.54;
+	   char Grade = 'A';
+
+	   cout << " My name is  : " <<name << endl;
+	   cout << " My scores1  : " << scores1 << " points " << endl;
+	   cout << " My scores2  : " << scores2 <<  " points " << endl;
+	   cout << " Total       : " << scores1+scores2  << " points" << endl;
+	   cout << " Grade       : " << Grade << endl;
+	   cout << " GPA         : " << GPA << endl;
+	   return(0);
+
+}
